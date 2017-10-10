1036c1a1123548578cc1bcbdbadb8f6819d465f7
Alex Timofeev <atimofeev@reactant.ru>
2017-10-10 13:38:48 +0700

    Be able to search by place name and redirect to the map page

---
+++ app/assets/javascripts/roofs.js
@@ -0,0 +1,33 @@
+roofs = {}
+
+roofs.init = function() {
+  this.$form  = $('#roofs-form')
+  this.$input = $('#roofs-autocomplete')
+
+  this.listenSubmit()
+  this.initAutocomplete()
+}
+
+roofs.initAutocomplete = function() {
+  this.autocomplete = new google.maps.places.Autocomplete(this.$input[0])
+  this.autocomplete.addListener('place_changed', roofs.fillInAddress)
+}
+
+roofs.listenSubmit = function() {
+  roofs.$form.submit(function() {
+    if (roofs.place) {
+      window.location.href = '/roofs/' + roofs.place[2]
+    }
+
+    return false
+  })
+}
+
+roofs.fillInAddress = function() {
+  var place = roofs.autocomplete.getPlace()
+  var geo   = place.geometry
+
+  if (geo) {
+    roofs.place = [geo.location.lat(), geo.location.lng(), roofs.$input.val()]
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'roofs#index'
+  resources :roofs, only: %i(index show create)
 end
