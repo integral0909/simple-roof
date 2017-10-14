ef6324ebf739163ed04ea3993d908732ac558f5b
Alex Timofeev <atimofeev@reactant.ru>
2017-10-14 15:05:43 +0700

    Draw poly

--- app/assets/javascripts/roofs.js
+++ app/assets/javascripts/roofs.js
@@ -33,9 +33,12 @@ roofs.fillInAddress = function() {
 }
 
 roofs.initMap = function() {
-  $map = $('#roofs-map')
+  $map                = $('#roofs-map')
+  this.markers        = []
+  this.calculatedPoly = null
+
   if ($map.length) {
-    var mymap = new L.Map($map[0], {
+    this.myMap = new L.Map($map[0], {
       center: new L.LatLng($map.data('lat'), $map.data('lng')),
       zoom:   20
     })
@@ -43,26 +46,38 @@ roofs.initMap = function() {
     L.gridLayer.googleMutant({
         type:    'satellite',
         maxZoom: 23
-    }).addTo(mymap)
+    }).addTo(this.myMap)
 
-    mymap.on('click', roofs.addMarker.bind(this))
+    this.myMap.on('click', roofs.addMarker.bind(this))
   }
 }
 
 roofs.addMarker = function(event) {
-  var newMarker = new L.marker(event.latlng, {
+  var marker = new L.marker(event.latlng, {
     draggable: 'true',
     icon: L.divIcon({
       className:  'leaflet-mouse-marker',
       iconAnchor: [6, 6],
       iconSize:   [12, 12]
     })
-  })
+  }).addTo(roofs.myMap)
+
+  roofs.markers.push(marker)
+  roofs.drawPoly()
 
   //this.markerList.push(newMarker)
   //this.drawPoly(this.markerList)
 
   return this
+}
+
+roofs.drawPoly = function() {
+  var coordinates = roofs.markers.map(function(value, index){ return [value.getLatLng().lat, value.getLatLng().lng] })
+  var polygones   = coordinates.concat([new L.LatLng(coordinates[0][0], coordinates[0][1])])
+
+  if (this.calculatedPoly !== null) this.calculatedPoly.removeFrom(roofs.myMap)
+
+  this.calculatedPoly = L.polyline(polygones, { 'fill': true }).addTo(roofs.myMap)
 }
 
 $(function() {
